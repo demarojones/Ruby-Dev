6ea6836da40569d4c7993d0cd2535ce64e4a5731
Demaro Jones <demaro4@gmail.com>
2015-10-28 08:55:18 -0400

    Refined Game and GameBoard Classes

--- Tic-Tac-Toe-Game/tictactoe.rb
+++ Tic-Tac-Toe-Game/tictactoe.rb
@@ -1,3 +1,5 @@
+
+#Player Class
 class Player
 	attr_accessor :name, :marker
 
@@ -7,13 +9,16 @@ class Player
 	end
 end
 
+
+
+
+#GameBoad Class
 class GameBoard
 	MAX_INDEX = 2
-	PLACEHOLDER = "__"
-	@current_player
-	def initialize(curr_players)
-		@current_players = curr_players
-		@current_player = @current_players[0]
+	PLACEHOLDER = "_"
+
+	def initialize(current_plyr)
+		@current_player = current_plyr
 		@game_board = create_board()
 	end
 	
@@ -60,38 +65,19 @@ class GameBoard
 		end
 	end
 	
-	def next_player
-		if(@current_player == @current_players[0])
-			curr_player = @current_players[1]
-		else
-			curr_player = @current_players[0]
-		end
-		return curr_player
-	end
-	
 	def size
 		@game_board.length
 	end
 	
-	def winner?
-		win = check_horizontal_lines
-		if(win)
-			return win
-		end
-		win = check_vertical_lines
-		if(win)
-			return win
-		end
-		if(win)
-			return win
-		end
-		win = check_diagonal_lines
-		if(win)
-			return win
-		end
-		return
+	def winner_found?
+		check_for_winner
 	end
-	#*****Privates******#
+
+	def player
+		@current_player
+	end
+
+	#*****Private parts******#
 	private 
 	def create_board
 		Array.new(MAX_INDEX+1){
@@ -100,108 +86,89 @@ class GameBoard
 	end
 
 	def validate_move(row,col)
-		if row <= @game_board.size and col <= @game_board.size
+		if row < @game_board.size and col < @game_board.size
 			if @game_board[row][col] == PLACEHOLDER
 				return true
 			else
-				puts "Dude, what board are you looking at? Try again"
+				puts "Dude, The space is already taken, Try again"
 			end
 		else
-			puts "Umm OK, you wanna check the instructions again?"
+			puts "That number is out of range, Please enter a number from 1-9!"
 		end
 		return false
 	end
 
-	def check_horizontal_lines
-		winner = false
-		for i in 0..MAX_INDEX
-			if @game_board[0][i] == @current_player.marker && @game_board[1][i] == @current_player.marker && @game_board[2][i] == @current_player.marker
-				puts "#{@current_player.name}, is the Winner!!"
-				winner = true
+	def check_for_winner
+		winning_combo = @current_player.marker * (MAX_INDEX+1)
+		diag_1=""
+		diag_2=""
+		init_sym = @game_board[0][0] #From top Left
+		for idx in 0..MAX_INDEX
+			diag_1 += @game_board[idx][idx]
+			diag_2 += @game_board[idx][MAX_INDEX - idx]
+			if((@game_board[0][idx] + @game_board[1][idx] + @game_board[2][idx])== winning_combo)
+				return true
+			elsif (@game_board[idx][0] + @game_board[idx][1] + @game_board[idx][2])== winning_combo
+				return true
 			end
 		end
-		return winner
-	end
-
-	def check_vertical_lines
-		winner = false
-		for i in 0..MAX_INDEX
-			if @game_board[i][0] == @current_player.marker && @game_board[i][1] == @current_player.marker && @game_board[i][2] == @current_player.marker
-				puts "#{@current_player.name}, is the Winner!!"
-				winner = true
-			end
-		end
-		return winner
-	end
-
-	def check_diagonal_lines
-		init_sym = @game_board[0][0]
-		for idx in 1..MAX_INDEX
-			if(init_sym != @game_board[idx][idx])
-				break
-			elsif idx == MAX_INDEX and init_sym != PLACEHOLDER
-				return init_sym
-			end
-		end
-		init_sym = @game_board[0][MAX_INDEX]
-		row_idx = 0
-		col_idx = MAX_INDEX
-		while row_idx < MAX_INDEX
-			row_idx = row_idx + 1
-			col_idx = col_idx - 1
-			if init_sym != @game_board[row_idx]
-				break
-			elsif row_idx == MAX_INDEX and init_sym != PLACEHOLDER
-				return init_sym
-			end
+		if diag_1 == winning_combo or diag_2 == winning_combo
+				return true
 		end
 		return false
 	end
-end
+end #End Class 'GameBoard'
 
+
+#Game Class
 class Game
 	@arr_players = []
-	def initialize(player_name_1, player_name_2)
-		@players = [player_name_1, player_name_2]
+	def initialize(player_1, player_2)
+		@players = [player_1.capitalize, player_2.capitalize]
 	end
 
 	def play_game(player_name)
-		if is_valid_player(player_name)
-			puts "#{player_name} is a valid player"
+		if is_valid_player(player_name.capitalize)
 			player_idx = get_player_index(player_name)
-			puts "#{@players[player_idx]} is a valid player with index #{player_idx}"
 			player_o = Player.new(@players[player_idx],"O")
-			puts "#{player_o.name} is player O"
 			if player_idx == 0
 				nm = @players[1]
 			else
 				nm = @players[0]
 			end
-			puts "The X player is #{nm}"
 			player_x = Player.new(nm, "X")
 			@arr_players = [player_o, player_x]
-			puts "#{@arr_players}"
+		else
+			puts "Umm, Who are you?... Please try again..."
+			return
 		end
 		@current_player = @arr_players[0]
 		
-		puts "The current player is #{@current_player}"
-		board = GameBoard.new(@arr_players)
-		puts "right before while loop! #{board}"
-		until board.winner? or board.full?
-			puts "until loop!"
+		board = GameBoard.new(@current_player)
+		until board.winner_found? or board.full?
 			board.get_player_move(@current_player)
-			@current_player = board.next_player()
+			@current_player = next_player()
 			board.display()
 			puts
 		end
-#
+
+		if board.winner_found?
+			puts "#{board.player.name}, you won!!!"
+		elsif board.full?
+			puts "It's a Tie!!!"
+		end
+			
 	end
 	
 
 	private 
-
-	def is_winner()
-		
+	def next_player
+		if(@current_player == @arr_players[0])
+			curr_player = @arr_players[1]
+		else
+			curr_player = @arr_players[0]
+		end
+		return curr_player
 	end
 	
 	def is_valid_player(p_name)
@@ -213,9 +180,5 @@ class Game
 	end
 end
 
-puts gm = Game.new("mike","david")
-
-gm.play_game("david")
-
-#gb = GameBoard.new()
-#puts gb.display()
+gm = Game.new("lindsay","jolie")
+gm.play_game("Jolie")
